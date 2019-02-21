c643a83d2bf5ea6be0294bb31cd93336eadc8f96
Achuth Rajula <achuthrajula1999@gmail.com>
2019-02-21 21:11:03 +0530

    Updates to events registration
    - fixed white space error in project expo page
    
    -linked respective registration pages to respective events
    
    -set minimum and maximum participants count in utilities/eventsMap
    
    -minor updates to over all events page

--- utilities/eventsMap.js
+++ utilities/eventsMap.js
@@ -3,72 +3,96 @@ module.exports = {
         id: "hackathon",
         title: 'Hackathon',
         price: 300,
-        date: "March 15 & 16, 2019"
+        date: "March 15 & 16, 2019",
+        requiredSize: 3,
+        maxsize: 5
     },
     landcircuit: {
         id: "landcircuit",
         title: "Land Circuit",
         price: 500,
-        date: "March 15, 2019"
+        date: "March 15, 2019",
+        requiredSize: 2,
+        maxsize: 3
     },
     drone: {
         id: "drone",
         title: "Drone Races",
         price: 500,
-        date: "March 16, 2019"
+        date: "March 16, 2019",
+        requiredSize: 1,
+        maxsize: 3
     },
     coding: {
         id: 'coding',
         title: 'Coding Competition',
         price: 500,
-        date: "March 16, 2019"
+        date: "March 16, 2019",
+        requiredSize: 1,
+        maxsize: 3
     },
     expo: {
         id: 'expo',
         title: 'Project Expo',
         price: 500,
-        date: "March 15, 2019"
+        date: "March 15, 2019",
+        requiredSize: 1,
+        maxsize: 3
     },
     counterstrike: {
         id: 'counterstrike',
         title: 'Counter Strike 1.6',
         price: 400,
-        date: "March 15 & 16, 2019"
+        date: "March 15 & 16, 2019",
+        requiredSize: 1,
+        maxsize: 1
     },
     pubg: {
         id: 'pubg',
         title: 'PUBG Mobile',
         price: 400,
-        date: "March 15 & 16, 2019"
+        date: "March 15 & 16, 2019",
+        requiredSize: 2,
+        maxsize: 4
     },
     adzap: {
         id: 'adzap',
         title: 'Ad - Zap',
         price: 400,
-        date: "March 15, 2019"
+        date: "March 15, 2019",
+        requiredSize: 2,
+        maxsize: 2
     },
     contrarian: {
         id: 'contrarian',
         title: 'Contrarian',
         price: 500,
-        date: "March 15, 2019"
+        date: "March 15, 2019",
+        requiredSize: 2,
+        maxsize: 2
     },
     manager: {
         id: 'manager',
         title: 'Young Manager',
         price: 300,
-        date: "March 16, 2019"
+        date: "March 16, 2019",
+        requiredSize: 1,
+        maxsize: 1
     },
     roadies: {
         id: 'roadies',
         title: 'Corporate Roadies',
         price: 400,
-        date: "March 16, 2019"
+        date: "March 16, 2019",
+        requiredSize: 2,
+        maxsize: 2
     },
     esprit: {
         id: 'esprit',
         title: 'Espirit de Corps',
         price: 300,
-        date: "March 15, 2019"
+        date: "March 15, 2019",
+        requiredSize: 2,
+        maxsize: 2
     }
 };
